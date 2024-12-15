dccda94b04f7d2fab4a7c1efd50d8e37acf6a0ae
Shrey Patel <87133722+Shreypatel2224@users.noreply.github.com>
2024-12-14 20:59:49 -0500

    Update BlockchainProject.rkt

--- BlockchainProject.rkt
+++ BlockchainProject.rkt
@@ -1,4 +1,4 @@
-;; ACCEL FUNDIES 2
+;; ACCEL FUNDIES 2 FINAL PROJECT
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname hwA_2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
